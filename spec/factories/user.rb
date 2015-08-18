8cb91961713fb705e29a5947f6e35348bd7b4321
Jan Kroezen <jankroezen@me.com>
2015-08-18 21:35:59 +0100

    Create user factory with Factory Girl, and DRY up sign up spec.

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name "Jan Kroezen"
+    email "jankroezen@me.com"
+    password "helloworld"
+    password_confirmation "helloworld"
+  end
+
+end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -3,63 +3,45 @@ require 'rails_helper'
 feature "Visitor signs up for an account" do
 
   before do
-    visit root_path
-    within '.welcome-box' do
-      click_link('Sign Up')
-    end
-
-    fill_in('Name', :with => 'Duplicate User')
-    fill_in('Email', :with => 'duplicate@me.com')
-    fill_in('Password', :with => 'helloworld')
-    fill_in('Password confirmation', :with => 'helloworld')
-    click_button('Sign up')
-
-    expect(page).to have_content('A message with a confirmation link has been sent to your email address.')
+    @valid_user = build(:user)
+    @invalid_user = build(:user, email: 'jankroezen')
   end
 
   scenario "Visitor successfully signs up using a username, email, and password" do
-    visit root_path
-    within '.welcome-box' do
-      click_link('Sign Up')
-    end
-
-    fill_in('Name', :with => 'Jan Kroezen')
-    fill_in('Email', :with => 'jankroezen@me.com')
-    fill_in('Password', :with => 'helloworld')
-    fill_in('Password confirmation', :with => 'helloworld')
-    click_button('Sign up')
-
+    visit_signup_page
+    fill_in_form_and_submit(@valid_user)
     expect(page).to have_content('A message with a confirmation link has been sent to your email address.')
   end
 
   scenario "Visitor signs up with an already registered email address" do
-    visit root_path
-    within '.welcome-box' do
-      click_link('Sign Up')
+    2.times do
+      visit_signup_page
+      fill_in_form_and_submit(@valid_user)
     end
-
-    fill_in('Name', :with => 'Jan Kroezen')
-    fill_in('Email', :with => 'duplicate@me.com')
-    fill_in('Password', :with => 'helloworld')
-    fill_in('Password confirmation', :with => 'helloworld')
-    click_button('Sign up')
-
     expect(page).to have_content('Email has already been taken')
   end
 
   scenario "Visitor signs up with an invalid email address" do
+    visit_signup_page
+    fill_in_form_and_submit(@invalid_user)
+    expect(page).to have_content('Email is invalid')
+  end
+
+  private
+
+  def visit_signup_page
     visit root_path
     within '.welcome-box' do
       click_link('Sign Up')
     end
+  end
 
-    fill_in('Name', :with => 'Jan Kroezen')
-    fill_in('Email', :with => 'jankroezen')
-    fill_in('Password', :with => 'helloworld')
-    fill_in('Password confirmation', :with => 'helloworld')
+  def fill_in_form_and_submit(test_user)
+    fill_in('Name', :with => test_user.name)
+    fill_in('Email', :with => test_user.email)
+    fill_in('Password', :with => test_user.password)
+    fill_in('Password confirmation', :with => test_user.password_confirmation)
     click_button('Sign up')
-
-    expect(page).to have_content('Email is invalid')
   end
 
 end
