fb02fe77a2adb96dcae3c902a1e1999a062f0f9c
Jan Kroezen <jankroezen@me.com>
2015-08-18 23:53:27 +0100

    Write sign in and out tests

---
+++ spec/features/user_sign_in_out_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+feature "User sign in and sign out" do
+
+  before do
+    @user = create(:user)
+    @invalid_user = build(:user, password: 'incorrect password')
+  end
+
+  scenario "user successfully signs in with email and password" do
+    visit_sign_in_page
+    fill_in_form_and_submit(@user)
+    expect(page).to have_content('successfully signed in')
+  end
+
+  scenario "user unsuccessfully signs in with invalid password" do
+    visit_sign_in_page
+    fill_in_form_and_submit(@invalid_user)
+    expect(page).to have_content('invalid email or password')
+  end
+
+  scenario "user can see whether or not she is signed in at the top of page" do
+    visit_sign_in_page
+    fill_in_form_and_submit(@user)
+    expect(page).to have_content("Hi #{@user.name}!")
+  end
+
+  scenario "user can sign out" do
+    visit_sign_in_page
+    fill_in_form_and_submit(@user)
+    click_link('Sign Out')
+    expect(page).to have_content('successfully signed out')
+  end
+
+  private
+
+  def visit_sign_in_page
+    visit root_path
+    within '.welcome-box' do
+      click_link 'Sign In'
+    end
+  end
+
+  def fill_in_form_and_submit(user)
+    fill_in('Email', :with => user.email)
+    fill_in('Password', :with => user.password)
+    click_button 'Sign in'
+  end
+
+end
