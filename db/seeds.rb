3b97779a85ceaa011dfcd8da0b9b517e2e2b1f83
Jan Kroezen <jankroezen@me.com>
2015-08-24 15:20:27 +0100

    Complete checkpoint 6, Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,56 @@
-# Create user
+# Create custom users
 
-user = User.new(
+me = User.new(
   name: 'Jan Kroezen',
   email: 'jankroezen@me.com',
   password: 'testpassword'
 )
-user.skip_confirmation!
-user.save
+me.skip_confirmation!
+me.save!
+
+heisenberg = User.new(
+  name: 'Walter White',
+  email: 'thecook@lospollos.net',
+  password: 'heisenberg'
+)
+heisenberg.skip_confirmation!
+heisenberg.save!
+
+smurf = User.new(
+  name: 'Papa Smurf',
+  email: 'thebigs@smurfvillage.io',
+  password: 'mushroom'
+)
+smurf.skip_confirmation!
+smurf.save!
+
+
+# Create additional users
+
+2.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
+
+# Create ToDo items
+
+50.times do
+  Item.create!(
+    user: users.sample,
+    name: Faker::Lorem.sentence
+  )
+end
+
+
+# How many Users and Items created?
 
 puts "#{User.count} users created."
+puts "#{Item.count} to-do items created."
